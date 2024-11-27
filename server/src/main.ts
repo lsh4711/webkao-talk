7f7e00ffb56c1547a0b66b482f4d2690d9fe7fec
lsh4711 <dowkwll@naver.com>
2024-11-27 15:39:33 +0900

    chore(server): cors origin 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'http://localhost:8081',
+    ],
+    credentials: true,
+  });
+
   SwaggerUtil.runtimeSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
